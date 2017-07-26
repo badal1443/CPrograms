3bbd47efc56cce250bf419e24d799b443192b8df
Sayali Singh <sayali@localhost.localdomain>
2017-07-25 22:22:35 -0700

    Commiting program for string manipulation

---
+++ string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+
+char c = 'A';
+char name[] = "Badal Singh";
+printf("The chracter is %c",c);
+printf("\n");
+printf("My name is %s",name);
+printf("\n\n");
+
+}
